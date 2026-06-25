384d2b34348952d7b5fb61c9c3327d4cb92689db
Afnankumbari <afnankumbari313@gmail.com>
2026-06-25 23:52:25 +0530

    updated day 1

---
+++ java/day 1/datatypes.java
@@ -0,0 +1,39 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 30000;
+        int i = 100000;
+        long l = 9999999999L;
+        float f = 3.14f;
+        double d = 3.14159265;
+        char c = 'J';
+        boolean bool = true;
+        String str = "Java";
+
+        System.out.println("byte:    " + b);
+        System.out.println("short:   " + s);
+        System.out.println("int:     " + i);
+        System.out.println("long:    " + l);
+        System.out.println("float:   " + f);
+        System.out.println("double:  " + d);
+        System.out.println("char:    " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String:  " + str);
+    }
+}
+
+
+
+
+// result
+/*
+ byte:    100
+short:   30000
+int:     100000
+long:    9999999999
+float:   3.14
+double:  3.14159265
+char:    J
+boolean: true
+String:  Java  
+ */
