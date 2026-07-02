0c9e9d99d2fa74bb273532ba9a00ca0aeae96dd8
Afnankumbari <afnankumbari313@gmail.com>
2026-07-03 01:22:41 +0530

    updated

---
+++ java/day3/Relational.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Relational {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 5;
+
+        boolean result1 = num1 > num2;
+        boolean result2 = num1 < num2;
+        boolean result3 = num1 >= num2;
+        boolean result4 = num1 <= num2;
+        boolean result5 = num1 == num2;
+        boolean result6 = num1 != num2;
+        boolean result7 = num1 > 0 && num2 > 0;
+        boolean result8 = num1 > 0 || num2 > 0;
+
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+    }
+    
+}
--- java/day3/arithmetic.java
+++ java/day3/arithmetic.java
@@ -1,3 +1,4 @@
+package day3;
 public class arithmetic {
     public static void main(String[] args) {
         
