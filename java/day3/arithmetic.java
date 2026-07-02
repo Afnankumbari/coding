2cdf43bba69e643081b2c6c48db93cd30fe6d47d
Afnankumbari <afnankumbari313@gmail.com>
2026-07-02 23:56:54 +0530

    day 3 added

---
+++ java/day3/arithmetic.java
@@ -0,0 +1,33 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        
+     int num1 = 7;
+     int num2 = 5;
+     
+     int result = num1 + num2;
+     int result1 = num1 - num2;
+     int result2 = num1 * num2;
+    int result3 = num1 / num2;
+    int result4 = num1 % num2;
+    int result5 = num1++;
+    int result6 = num2--;
+    int result7 = ++num1;
+    int result8 = --num2;
+    int result9 = num1 + num2 * 2;
+    int result10 = (num1 + num2) * 2;
+
+    
+     System.out.println(result);
+     System.out.println(result1);
+     System.out.println(result2);
+     System.out.println(result3);
+     System.out.println(result4);
+     System.out.println(result5);
+     System.out.println(result6);
+     System.out.println(result7);
+     System.out.println(result8);
+     System.out.println(result9);
+     System.out.println(result10);
+    }
+    
+}
