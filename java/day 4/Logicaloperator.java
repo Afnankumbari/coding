6041188d07c245ee01ad9c24187130bb64765f75
Afnankumbari <afnankumbari313@gmail.com>
2026-07-04 22:02:46 +0530

    day 4 updated

---
+++ java/day 4/Logicaloperator.java
@@ -0,0 +1,14 @@
+class Logicaloperator{
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+
+        boolean result1 =(x > y)|(a < b);
+         boolean result2 =(x > y)&(a < b);
+        System.out.println(result1 + ", " + result2);
+
+
+    }
+}
---
+++ java/day 4/Task.java
@@ -0,0 +1,28 @@
+class Task {
+
+  public static void main(String[] args){
+      
+       // write code here
+      int a = 20;
+      int b = 10;
+      
+     int add = a + b;
+      int substract = a - b;
+      int multiply = a * b;
+      int divide = a/b;
+    
+      boolean compare = a > b;
+        
+      boolean result4 = (a > b)&&(b > 0);
+      
+      
+      System.out.println("Addition:"+" "+add);
+      System.out.println("Subtraction:"+" "+substract);
+      System.out.println("Multiplication:"+" "+multiply);
+      System.out.println("Division:"+" "+divide);
+      System.out.println("Is a greater than b?"+" "+compare);
+      System.out.println("Is a > b and b > 0?"+" "+result4);
+  
+      
+  }
+}
