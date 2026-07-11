1fdcd9d533b24f5bac5ed69e0e5aaf6a0729a2f7
Afnankumbari <afnankumbari313@gmail.com>
2026-07-11 16:53:30 +0530

    task updated

---
+++ java/day6/TeluskoTask1.java
@@ -0,0 +1,40 @@
+package day6;
+
+public class TeluskoTask1 {
+    public static void main(String[] args) {
+
+        //For Loop
+        
+        System.out.print("For Loop: ");
+
+
+        for(int i=1; i<=5; i++)
+            {
+            System.out.print(i +" ");
+        }
+        //While Loop
+
+        System.out.println();
+        System.out.print("While Loop: ");
+
+        int i=1;
+        while(i<=5){
+            System.out.print(i + " ");
+            i++;
+        }
+
+
+        // Do - While loop
+        System.out.println();
+        System.out.print("Do-While Loop: ");
+
+        
+         int j = 1;  
+        do{
+            System.out.print(j +" ");
+            j++;
+        }
+        while(j <= 5);
+    }
+    
+}
